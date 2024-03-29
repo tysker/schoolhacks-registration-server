e725170edf53866560e289886c9c6d9c2a8ede06
tysker <joergoertel@proton.me>
2024-03-29 11:38:31 +0100

    refactored and clean-up

--- utility/email.ts
+++ utility/email.ts
@@ -9,38 +9,36 @@ type EmailOptions = {
     message: string;
 };
 const sendEmail = async (options: EmailOptions) => {
-
-    // 1) create a transporter
-    const transporter = nodemailer.createTransport({
-        // @ts-ignore
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        tls: {
-            ciphers: process.env.EMAIL_CIPHERS,
-        },
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-
-    });
-
-    // 2) define email options
-    const mailOptions = {
-        from: "SchoolHacks <tyskerjoerg@outlook.com>",
-        to: options.email,
-        subject: options.subject,
-        html: options.message,
-    };
-
-
-    // 3) send the email
     try {
-        await transporter.sendMail(mailOptions);
-    } catch (e: any) {
-        throw new Error(e.message);
-    }
+        // 1) create a transporter
+        const transporter = nodemailer.createTransport({
+            // @ts-ignore
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            tls: {
+                ciphers: process.env.EMAIL_CIPHERS,
+            },
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
 
+        // 2) define email options
+        const mailOptions = {
+            from: "SchoolHacks <tyskerjoerg@outlook.com>",
+            to: options.email,
+            subject: options.subject,
+            html: options.message,
+        };
+
+        // 3) send the email
+        await transporter.sendMail(mailOptions);
+
+    } catch (error) {
+        console.error("Error sending email:", error);
+        throw new Error("Failed to send email. Please try again later.");
+    }
 };
 
 export default sendEmail;
